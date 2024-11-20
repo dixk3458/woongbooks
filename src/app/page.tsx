308867ddc29a3b545ebddda5b7635bfde671f524
Jaewoong <dixk3458@naver.com>
2024-11-20 22:05:18 +0900

    fetch books & UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,61 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import Image from 'next/image';
+import { BookData } from '../types/book';
+import styles from './page.module.css';
+import Link from 'next/link';
+import BookItem from '@/components/book-item';
 
-export default function Home() {
+async function AllBooks() {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`
+  );
+
+  if (!response.ok) {
+    return <div>오류가 발생했습니다.</div>;
+  }
+
+  const books: BookData[] = await response.json();
+
   return (
     <div>
-     Home
+      {books.map(book => (
+        <BookItem book={book} key={book.id} />
+      ))}
+    </div>
+  );
+}
+
+async function RecoBooks() {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/random`
+  );
+
+  if (!response.ok) {
+    return <div>오류가 발생했습니다.</div>;
+  }
+
+  const books: BookData[] = await response.json();
+
+  return (
+    <div>
+      {books.map(book => (
+        <BookItem book={book} key={book.id} />
+      ))}
+    </div>
+  );
+}
+
+export default async function Home() {
+  return (
+    <div className={styles.container}>
+      <div>임시 서치바</div>
+      <section>
+        <h3>지금 추천하는 도서</h3>
+        <RecoBooks />
+      </section>
+      <section>
+        <h3>등록된 모든 도서</h3>
+        <AllBooks />
+      </section>
     </div>
   );
 }
---
+++ src/components/book-item.tsx
@@ -0,0 +1,27 @@
+import { BookData } from '@/types/book';
+import Image from 'next/image';
+import Link from 'next/link';
+
+import styles from './book-item.module.css';
+
+export default function BookItem({ book }: { book: BookData }) {
+  const { id, title, description, subTitle, author, coverImgUrl, publisher } =
+    book;
+
+  return (
+    <Link className={styles.container} href={`/book/${id}`}>
+      <Image
+        src={coverImgUrl}
+        alt={`${title}의 표지 이미지`}
+        width={80}
+        height={105}
+      />
+      <div>
+        <div className={styles.title}>{title}</div>
+        <div className={styles.subTitle}>{subTitle}</div>
+        <br />
+        <div className={styles.author}>{`${author} | ${publisher}`}</div>
+      </div>
+    </Link>
+  );
+}
